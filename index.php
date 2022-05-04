6f0543ee75896b7950aefedab6e6daf4086c99bb
sharathkv <sharathkv@dckap>
2022-05-04 16:30:04 +0530

    90224

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-  CURLOPT_URL => "https://api.bigcommerce.com/stores/store_hash/v3/catalog/products/product_id/metafields",
+  CURLOPT_URL => "https://api.bigcommerce.com/stores/store_hash/v3/catalog/products/90224/metafields",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n  \"id\": 6,\n  \"permission_set\": \"app_only\",\n  \"namespace\": \"Warehouse Locations\",\n  \"key\": \"Location\",\n  \"value\": \"4HG\",\n  \"description\": \"Location in the warehouse\",\n  \"resource_type\": \"product\",\n  \"resource_id\": 111,\n  \"date_created\": \"2018-05-07T20:14:17.000Z\",\n  \"date_modified\": \"2018-05-07T20:14:17.000Z\"\n}",
+  CURLOPT_POSTFIELDS => "{\n  \"permission_set\": \"app_only\",\n  \"namespace\": \"Warehouse Locations\",\n  \"key\": \"Location\",\n  \"value\": \"4HG\",\n  \"description\": \"Location in the warehouse\",\n  \"resource_type\": \"product\",\n  \"resource_id\": 111,\n  \"date_created\": \"2018-05-07T20:14:17.000Z\",\n  \"date_modified\": \"2018-05-07T20:14:17.000Z\"\n}",
   CURLOPT_HTTPHEADER => [
     "Accept: application/json",
     "Content-Type: application/json",
